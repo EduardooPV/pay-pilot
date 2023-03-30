e6c0f04295129ebfef78c6f6d44a8501872ebc55
EduardooPV <eduardoveltroni@hotmail.com>
2023-03-30 07:10:26 -0300

    feat: adiciona as rotas do usuario no express

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./useCases/User/authenticateUser/AuthenticateUserController";
 
 import { CreateUserController } from "./useCases/User/createUser/CreateUserController";
+import { RefreshTokenUserController } from "./useCases/User/refreshTokenUser/RefreshTokenUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
+const refreshTokenUserController = new RefreshTokenUserController();
 
 router.post("/user", createUserController.handle);
+router.post("/user/login", authenticateUserController.handle);
+router.post("/user/refresh-token", refreshTokenUserController.handle);
 
 export { router };
