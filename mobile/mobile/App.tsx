11bbb131f099654e188d9c4e1e1ad987ece70f7f
EduardooPV <eduardoveltroni@hotmail.com>
2023-03-30 07:35:26 -0300

    feat: adiciona font inter

--- mobile/mobile/App.tsx
+++ mobile/mobile/App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from "@expo-google-fonts/roboto";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
+      <Text>
         Open up App.tsx to start working on your app!!
       </Text>
       <StatusBar style="auto" />
