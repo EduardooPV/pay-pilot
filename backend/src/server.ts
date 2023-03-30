a5a2ceb7e53528286a1ec33d2c7b049ee9dc0757
EduardooPV <eduardoveltroni@hotmail.com>
2023-03-30 07:19:03 -0300

    feat: cria documentacao backend com swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import "express-async-errors";
 import express, { Request, Response, NextFunction } from "express";
+import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
