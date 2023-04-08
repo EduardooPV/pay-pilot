27cd5fe91ad09d15dfe256740a0f67df2c67faca
EduardooPV <eduardoveltroni@hotmail.com>
2023-04-08 15:54:34 -0300

    feat: cria componente summary

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,18 +1,12 @@
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  Alert,
-  StyleSheet,
-} from "react-native";
-import { Loading } from "../components/Loading";
+import { View, Text, Image, TouchableOpacity, Alert } from "react-native";
 import Welcome from "./Welcome";
 
-import SettingsImage from "../assets/settings.svg";
-import MoneyImage from "../assets/money.svg";
-import { UserAuth } from "../context/UserAuth";
+import { Loading } from "../components/Loading";
 import { UserTransactions } from "../context/GetTransactionsUser";
+import { UserAuth } from "../context/UserAuth";
+
+import SettingsImage from "../assets/settings.svg";
+import Summary from "../components/Summary";
 
 export default function Home() {
   const { isAuthenticated, isLoading } = UserAuth();
@@ -25,7 +19,7 @@ export default function Home() {
   } else if (isAuthenticated) {
     return (
       <View className="flex-1 w-full px-4 pt-[50] bg-background gap-y-7 relative">
-        <View className="flex flex-row items-center justify-between">
+        <View className="flex flex-row items-center justify-between mb-6">
           <View className="flex flex-row gap-x-3">
             <View className="w-[50] h-[50] border-primary300 rounded-full border-2 overflow-hidden">
               <Image
@@ -50,53 +44,10 @@ export default function Home() {
           </TouchableOpacity>
         </View>
 
-        <View>
-          <Text className="text-caption leading-caption text-neutral-400 mb-3">
-            Cálculo das transações
-          </Text>
-          <View>
-            <View className="justify-center items-center bg-primary300 p-5 rounded-lg">
-              <View className="flex-row items-center gap-x-4 mb-3">
-                <Text className="text-paragraph2 leading-paragraph2 text-white">
-                  Saldo disponível
-                </Text>
-                <MoneyImage />
-              </View>
-
-              <Text className="text-h4 leading-h4 font-bold text-white">
-                R$ 1.546,02
-              </Text>
-            </View>
-
-            <View>
-              <View
-                className="justify-center items-center p-2 space-y-2 bg-white rounded-lg"
-                style={styles.shadow}
-              >
-                <View className="flex-row items-center gap-x-4 mb-3">
-                  <Text className="text-paragraph2 leading-paragraph2 text-white">
-                    Saldo disponível
-                  </Text>
-                  <MoneyImage />
-                </View>
-
-                <Text className="text-h4 leading-h4 font-bold text-white">
-                  R$ 1.546,02
-                </Text>
-              </View>
-            </View>
-          </View>
-        </View>
+        <Summary />
       </View>
     );
   } else {
     return <Welcome />;
   }
 }
-
-const styles = StyleSheet.create({
-  shadow: {
-    elevation: 8,
-    shadowColor: "#0f0f0f61",
-  },
-});
